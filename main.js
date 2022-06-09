0f9f967b7c8ca4ae20cad33b133a0f0920c25611
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-06-09 16:56:02 +0200

    style des elevation profils geändert

--- main.js
+++ main.js
@@ -106,7 +106,12 @@ gpxTrack.on("loaded", function (evt) {
     gpxLayer.bindPopup(popup);
 })
 
-let elevationControl = L.control.elevation({}).addTo(map);
+let elevationControl = L.control.elevation({
+    time: false,
+    elevationDiv: "#profile",
+    theme: 'bike-tirol',
+    height: 200
+}).addTo(map);
 gpxTrack.on("addline", function(evt) {
     elevationControl.addData(evt.line);
 })
